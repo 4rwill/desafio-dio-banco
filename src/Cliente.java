111fda03d6f5f0dde5d905647e98acc742f9bd6c
4rwill <christian10will@gmail.com>
2024-07-01 19:45:01 -0300

    desafio

---
+++ src/Cliente.java
@@ -0,0 +1,13 @@
+public class Cliente {
+    //attributes
+    private String nome;
+
+    //getters and setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
